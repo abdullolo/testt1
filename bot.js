3ddea542dccaea23358fd6885f0a2fc5baa6e0a2
abdullolo <46109649+abdullolo@users.noreply.github.com>
2018-12-25 01:38:43 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("526425902733590540")
+setInterval(function() {
+channel.send(` V2VDONT LEMMIE LOLO GANG IS HERE 564578(&*^(878}"}"{MAfineafeafEOQRNEMQOR'SD;FASFSDFklDAM LONE `);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
